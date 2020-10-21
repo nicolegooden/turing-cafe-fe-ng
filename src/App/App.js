e85075e013d8297ce2d5aff0849b321fd7dc9567
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-10-21 10:06:28 -0600

    Add Form component to App's render

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { getExistingReservations } from '../apiCalls.js';
 import ResContainer from '../ResContainer/ResContainer.js';
+import Form from '../Form/Form.js';
 
 class App extends Component {
   constructor() {
@@ -21,7 +22,7 @@ class App extends Component {
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <Form />
         </div>
         <div className='resy-container'>
           <ResContainer existingReservations={this.state.existingReservations}/>
