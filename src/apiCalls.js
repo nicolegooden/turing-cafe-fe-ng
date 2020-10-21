e461853fd0038763a8158fd56081296554b52fca
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-10-21 11:39:26 -0600

    Create post request for new reservation

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,3 +3,18 @@ export const getExistingReservations = () => {
     .then(response => response.json())
     .catch(err => console.log(err))
 }
+
+export const postNewReservation = (name, date, time, number) => {
+  return fetch('http://localhost:3001/api/v1/reservations', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: name,
+      date: date,
+      time: time,
+      number: number
+    })
+  }).then(response => response.json()).catch(err => console.log(err))
+}
