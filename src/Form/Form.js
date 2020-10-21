3d3df8dc587be06c900d04517533359b9ccb83cd
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-10-21 10:27:04 -0600

    Create handleSubmission() to pass new reservation object to App's existingReservations

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -17,6 +17,20 @@ class Form extends Component {
     this.setState({[event.target.name]: event.target.value})
   }
 
+  handleSubmission = () => {
+    const newRes = {
+      name: this.state.name,
+      date: this.state.date,
+      time: this.state.time,
+      number: this.state.number
+      //Do I need to create my own id since a POST is not being made yet?
+    }
+    this.props.makeReservation(newRes);
+    this.setState({name: '', date: '', time: '', number: ''})
+    //I want to set the state of the number key back to null, but this does not
+    //clear the input on submission... will check back on this later if time permits.
+  }
+
   render() {
     return (
       <section>
@@ -24,7 +38,7 @@ class Form extends Component {
         <input type='text' onChange={this.handleChange} name='date' value={this.state.date} placeholder='Date (mm/dd)'></input>
         <input type='text' onChange={this.handleChange} name='time' value={this.state.time} placeholder='Time'></input>
         <input type='number' onChange={this.handleChange} name='number' value={this.state.number} placeholder='Number of guests'></input>
-        <button>Make Reservation</button>
+        <button onClick={this.handleSubmission}>Make Reservation</button>
       </section>
     )
   }
