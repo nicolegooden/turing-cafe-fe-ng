dac02bc0402eb0abbe1bff327323858571de0e26
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-10-21 10:14:23 -0600

    Track Form input changes via state

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -4,16 +4,26 @@ import './form.css';
 class Form extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      name: '',
+      date: '',
+      time: '',
+      number: null
+      //remember to use parseInt on this.state.number (comes in as string)
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({[event.target.name]: event.target.value})
   }
 
   render() {
     return (
       <section>
-        <input placeholder='Name'></input>
-        <input placeholder='Date (mm/dd)'></input>
-        <input placeholder='Time'></input>
-        <input placeholder='Number of guests'></input>
+        <input onChange={this.handleChange} name='name' value={this.state.name} placeholder='Name'></input>
+        <input onChange={this.handleChange} name='date' value={this.state.date} placeholder='Date (mm/dd)'></input>
+        <input onChange={this.handleChange} name='time' value={this.state.time} placeholder='Time'></input>
+        <input onChange={this.handleChange} name='number' value={this.state.number} placeholder='Number of guests'></input>
         <button>Make Reservation</button>
       </section>
     )
