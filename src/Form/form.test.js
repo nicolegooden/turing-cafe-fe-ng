d0203bfded86e85f9dc9928d5959f4a9ef0a8bd5
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-10-21 11:06:45 -0600

    Add unit test to ensure input values reflect user actions

--- src/Form/form.test.js
+++ src/Form/form.test.js
@@ -19,5 +19,28 @@ describe('Form', () => {
     expect(screen.getByPlaceholderText('Number of guests')).toBeInTheDocument();
     expect(screen.getByRole('button', { name: 'Make Reservation' })).toBeInTheDocument();
   })
+
+  it('should track changes based on user input', () => {
+    
+    const mockMakeReservation = jest.fn();
+
+    render(
+      <Form makeReservation={mockMakeReservation}/>
+    )
+
+    //put data into the form (each input) - DONE
+    //check that the value entered by the user is shown in the form input
+    //essentially, track changes with userEvent.type
+
+    userEvent.type(screen.getByPlaceholderText('Name'), 'Khaleta');
+    userEvent.type(screen.getByPlaceholderText('Date (mm/dd)'), '11/10');
+    userEvent.type(screen.getByPlaceholderText('Time'), '6:45');
+    userEvent.type(screen.getByPlaceholderText('Number of guests'), '5');
+
+    expect(screen.getByPlaceholderText('Name')).toHaveValue('Khaleta');
+    expect(screen.getByPlaceholderText('Date (mm/dd)')).toHaveValue('11/10');
+    expect(screen.getByPlaceholderText('Time')).toHaveValue('6:45');
+    expect(screen.getByPlaceholderText('Number of guests')).toHaveValue(5);
+  })
 })
 
